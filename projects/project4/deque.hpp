5fbad81ac85e4f331c2dd577c1d624f89db22a3b
cag185 <cag185@pitt.edu>
2021-04-03 22:27:32 -0400

    Added deque.hpp functions

--- projects/project4/deque.hpp
+++ projects/project4/deque.hpp
@@ -2,12 +2,51 @@
 #define DEQUE_HPP
 
 #include "abstract_deque.hpp"
+#include "Node.hpp"
 
 template <typename T>
 class Deque: public AbstractDeque<T>{
 
-  // TODO
-  
+  //----------//PRIVATE//----------//
+
+  //----------//PUBLIC//----------//
+
+  //---//ORIGINAL FUNCTIONS//---//
+
+  //CONSTRUCTOR//
+  Deque();
+
+  //DESTRUCTOR//
+ ~Deque();
+
+  //COPY ASSIGNMENT//
+  Deque& operator=(const Deque& x);
+
+  //COPY CONSTRUCTOR//
+  Deque(const Deque& x);
+
+  //---//TEMPLATE FUNCTIONS//---//
+
+  //returns true if the deque is empty
+  bool isEmpty();
+
+  //add item to the front of the deque
+  void pushFront(const T & item);
+
+  //remove an item at the front of a deque
+  void popFront();
+
+  //returns the item at the front of the deque
+  T front() const;
+
+  //at item to the back of the deque
+  void pushBack(const T & item);
+
+  //remove an item at the back of the deque
+  void popBack();
+
+  //returns the item at the back of the deque
+  T back() const;
 };
 
 #include "deque.tpp"
