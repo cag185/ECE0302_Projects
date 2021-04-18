5d8ea8ac14367125fb1afe25339fd9c66fc477c7
cag185 <cag185@pitt.edu>
2021-04-17 21:56:39 -0400

    Added in some functionality for Add

--- ECE0302_Projects-main/homework/hw10/heap_priority_queue/heap_priority_queue.h
+++ ECE0302_Projects-main/homework/hw10/heap_priority_queue/heap_priority_queue.h
@@ -27,6 +27,7 @@ private:
     DynamicArrayList<T> lst;
 };
 
+//DONE
 template <typename T>
 bool HeapPriorityQueue<T>::isEmpty()
 {
@@ -39,6 +40,30 @@ template <typename T>
 void HeapPriorityQueue<T>::add(const T& item)
 {
     //toDo
+    //need to make sure that when we add a value, the Binary Tree is balanced
+    //do this by inserting in the correct place
+    
+
+    //Case where we are inserting into an empty heap
+    if(lst.isEmpty())
+    {
+        lst.setEntry(0, item);
+        return;
+    }
+    //case where we are adding to the middle of a heap
+    for(int i = 0; i < lst.getLength(); i++)
+    {
+        if(item > lst.getEntry(i) && item < lst.getEntry(i+1))    //if item is between two points
+        //shift everything after i to i+1
+        for(int j = i+1; j < lst.getLength(); j++)
+        {
+            lst.setEntry(j, lst.getEntry(j+1)); //take every value and shift it by one to the right
+        }
+        //insert the item into the array at index i+1
+        lst.setEntry(i+1, item);
+        return;
+    }
+
 }
 
 template <typename T>
