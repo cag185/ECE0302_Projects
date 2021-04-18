9c58a8b2d50f5366f42a041dfd10f1d6f69e0984
cag185 <cag185@pitt.edu>
2021-04-18 03:46:47 -0400

    Everything works

--- ECE0302_Projects-main/homework/hw10/heap_priority_queue/heap_priority_queue.h
+++ ECE0302_Projects-main/homework/hw10/heap_priority_queue/heap_priority_queue.h
@@ -45,9 +45,8 @@ void HeapPriorityQueue<T>::add(const T& item)
     //Case where we are inserting into an empty heap
     if(lst.isEmpty())
     {
-        std::cout <<"here" << std::endl;
+        
         lst.insert(0, item);
-        std::cout << "worked" << std::endl;
         return;
     }
 
@@ -76,6 +75,7 @@ void HeapPriorityQueue<T>::add(const T& item)
             else
             {
                 //we have trickled up far enough
+                correctLoc = true;
                 return;
             }
         }
@@ -87,7 +87,101 @@ void HeapPriorityQueue<T>::add(const T& item)
 template <typename T>
 void HeapPriorityQueue<T>::remove()
 {
-    //todo
+    std::cout << "in remove" << std::endl;
+    //yoink the value at the end of the heap
+    lst.setEntry(0, lst.getEntry(lst.getLength()-1));
+    std::cout << "here" << std::endl;
+    //remove the item at the end of the heap
+    lst.remove(lst.getLength()-1);
+    std::cout << "here" << std::endl;
+
+    //need to check the children of the new root and see which is larger
+    //if the child is larger than the root, swap em
+    std::cout << "here" << std::endl;
+    int newRoot = 0;
+    int leftChild = newRoot * 2 + 1;
+    std::cout << "we made it here" << std::endl;
+    int rightChild = leftChild + 1;
+    T swapItem;
+    std::cout <<"we made it past intializing the children" << std::endl;
+    bool complete = false;
+    int count = 1;
+
+    //if the tree is single node
+    if(lst.getLength() == 1)
+    {
+        complete = true;
+        return;
+    }
+
+    //two nodes
+    if(lst.getLength() == 2)
+    {
+        if(lst.getEntry(0) < lst.getEntry(1))
+        {
+            //swap
+            swapItem = lst.getEntry(1);
+            lst.setEntry(1, lst.getEntry(0));
+            lst.setEntry(0, swapItem);
+            complete = true;
+            return;
+        }
+    }
+
+    //test left child and right child
+    if(lst.getLength() >= 3)
+    {
+    while(!complete)
+    {
+        //keep testind swappin
+        if(lst.getEntry(leftChild) > lst.getEntry(newRoot) && lst.getEntry(rightChild) < lst.getEntry(newRoot))
+        {
+            std::cout << "left child" << std::endl;
+            //swap the values -- parent and left child
+            swapItem = lst.getEntry(newRoot);
+            lst.setEntry(newRoot, lst.getEntry(leftChild));
+            lst.setEntry(leftChild, swapItem);
+            //reset the root
+            newRoot = leftChild;
+            leftChild = newRoot * 2 + 1;
+            rightChild = leftChild + 1;
+            std::cout << "New Root: " << lst.getEntry(newRoot) << std::endl;
+            std::cout << "Left Child: " << lst.getEntry(leftChild) << std::endl;
+            std::cout << "Right Child: " << lst.getEntry(rightChild) << std::endl;
+
+        }
+        else if(lst.getEntry(rightChild) > lst.getEntry(newRoot) && lst.getEntry(leftChild) < lst.getEntry(newRoot))
+        {
+            std::cout << "right child" << std::endl;
+            //swap the values -- parent and right child
+            swapItem = lst.getEntry(newRoot);
+            lst.setEntry(newRoot, lst.getEntry(rightChild));
+            lst.setEntry(rightChild, swapItem);
+            //reset the root
+            newRoot = rightChild;
+            leftChild = newRoot * 2 + 1;
+            rightChild = leftChild + 1;
+            std::cout << "New Root: " << lst.getEntry(newRoot) << std::endl;
+            std::cout << "Left Child: " << lst.getEntry(leftChild) << std::endl;
+            std::cout << "Right Child: " << lst.getEntry(rightChild) << std::endl;
+        }
+        //check to make sure the root is the greatest
+        for(int i = 1; i < lst.getLength(); i++)
+        {
+            if(lst.getEntry(0) >lst.getEntry(i))
+            {
+                count++;
+            }
+        }
+        //if the head is greater than all other values
+        if(count == lst.getLength())
+        {
+            complete = true;
+            return;
+        }
+    }    
+    }
+
 }
 
 template <typename T>
